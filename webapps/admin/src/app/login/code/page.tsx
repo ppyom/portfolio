9802be6bca6e275cfbc04a9bda97dc9fce3c2a25
ppyom <ouob77@kakao.com>
2024-12-30 16:57:55 +0900

    design: 코드 발급 페이지 퍼블리싱

---
+++ webapps/admin/src/app/login/code/page.tsx
@@ -0,0 +1,21 @@
+import { Button, Input, Typography } from '@packages/ui/atoms';
+import Link from 'next/link';
+
+const CodePage = () => {
+  return (
+    <div className="flex flex-col gap-2">
+      <Input placeholder="이메일 주소 입력" />
+      <Button bg="secondary">로그인 코드 발급</Button>
+      <Link className="self-end" href="/login" replace>
+        <Typography
+          type="caption"
+          className="text-brand-primary-dark duration-200 hover:text-brand-secondary-dark"
+        >
+          이미 코드가 있으신가요?
+        </Typography>
+      </Link>
+    </div>
+  );
+};
+
+export default CodePage;
