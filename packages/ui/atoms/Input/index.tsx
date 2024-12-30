d812d664c4349b1fcddabb8a420f1115dd7b3eb3
ppyom <ouob77@kakao.com>
2024-12-30 16:51:13 +0900

    design: Input 컴포넌트 구현

---
+++ packages/ui/atoms/Input/index.tsx
@@ -0,0 +1,39 @@
+import type { ComponentPropsWithoutRef } from 'react';
+import { cn } from '../../utils';
+
+interface Props
+  extends Pick<
+    ComponentPropsWithoutRef<'input'>,
+    'value' | 'onChange' | 'placeholder' | 'name' | 'id' | 'disabled'
+  > {
+  className?: string;
+}
+
+export const Input = ({
+  value,
+  onChange,
+  placeholder,
+  className,
+  disabled,
+  ...props
+}: Props) => {
+  return (
+    <input
+      {...props}
+      className={cn(
+        'outline-none',
+        'px-3 py-2.5',
+        'rounded-lg text-sm',
+        'bg-base-white border-[1px] duration-200',
+        'placeholder:text-neutral-400',
+        'border-brand-primary placeholder-shown:border-neutral-200 focus:placeholder-shown:border-neutral-300',
+        'disabled:bg-neutral-100 disabled:border-neutral-300',
+        className,
+      )}
+      value={value}
+      onChange={onChange}
+      placeholder={placeholder}
+      disabled={disabled}
+    />
+  );
+};
