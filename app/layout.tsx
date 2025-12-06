c0f3c3a3a3c54069fa697d83eaacf5328603682c
ppyom <ouob77@kakao.com>
2025-12-06 17:35:58 +0900

    🚚 사용자 페이지를 라우터 그룹으로 구성

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,7 +40,6 @@ export default function RootLayout({ children }: Readonly<Props>) {
           disableTransitionOnChange
         >
           {children}
-          <Navigation />
           <Footer />
         </ThemeProvider>
       </body>
