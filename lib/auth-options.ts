052cdce744119a2acff64f4e036cdd5ff0f8a1b5
ppyom <ouob77@kakao.com>
2025-12-06 17:31:03 +0900

    ✨ next-auth 초기 설정 / 로그인 로직 구현

---
+++ lib/auth-options.ts
@@ -0,0 +1,72 @@
+import * as bcrypt from 'bcrypt';
+import { NextAuthOptions, Session } from 'next-auth';
+import { JWT } from 'next-auth/jwt';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import { eq } from 'drizzle-orm';
+import { db } from '@/database';
+import { userTable } from '@/database/schema';
+
+const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        username: { label: 'username', type: 'text' },
+        password: { label: 'password', type: 'password' },
+      },
+      async authorize(credentials) {
+        if (!credentials?.username || !credentials?.password) {
+          return null;
+        }
+
+        const [user] = await db
+          .select()
+          .from(userTable)
+          .where(eq(userTable.username, credentials.username));
+
+        if (!user) {
+          return null;
+        }
+
+        const isValid = await bcrypt.compare(
+          credentials.password,
+          user.password,
+        );
+
+        if (!isValid) {
+          return null;
+        }
+
+        return {
+          id: user.id,
+          username: user.username,
+          name: user.name,
+          admin: user.admin,
+        };
+      },
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+  session: {
+    strategy: 'jwt',
+    maxAge: 7 * 24 * 60 * 60, // 7일
+  },
+  callbacks: {
+    async jwt(params) {
+      const { token, user } = params;
+      if (user) {
+        token.user = user;
+      }
+      return token;
+    },
+    async session(params: { session: Session; token: JWT }) {
+      const { session, token } = params;
+      if (token) {
+        session.user = token.user;
+      }
+      return session;
+    },
+  },
+};
+
+export default authOptions;
