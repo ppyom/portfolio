43e7e7a27f33135abb914899a898aaf9c6cc862c
ppyom <ouob77@kakao.com>
2025-12-07 00:39:16 +0900

    🗃️ 프로젝트 관련 스키마 작성 및 Drizzle 마이그레이션

--- database/schema.ts
+++ database/schema.ts
@@ -1,5 +1,6 @@
 import { sql } from 'drizzle-orm';
 import { pgTable, boolean, text, uuid } from 'drizzle-orm/pg-core';
+import { jsonb } from 'drizzle-orm/pg-core/columns/jsonb';
 
 export const userTable = pgTable('user', {
   id: uuid('id')
@@ -9,4 +10,46 @@ export const userTable = pgTable('user', {
   password: text('password').notNull(),
   name: text('name'),
   admin: boolean().default(false),
+});
+
+export const projectTable = pgTable('project', {
+  id: uuid('id')
+    .primaryKey()
+    .default(sql`gen_random_uuid()`),
+  title: text('title').notNull(),
+  description: text('description'),
+  category: text('category'),
+  coverImageUrl: text('cover_image_url'),
+  githubUrl: text('github_url'),
+  applicationUrl: text('application_url'),
+  tags: text('tags').array(),
+  overview: text('overview'),
+  features: text('features').array(),
+  images: text('images').array(),
+  goals: text('goals').array(),
+  results: text('results').array(),
+  member: jsonb('member').$type<{
+    size: number;
+    role: string;
+    responsibilities: string[];
+  }>(),
+  createdAt: text('created_at')
+    .notNull()
+    .$default(() => sql`NOW()`),
+  updatedAt: text('updated_at').$onUpdate(() => sql`NOW()`),
+});
+
+export const projectTechStackTable = pgTable('project_tech_stack', {
+  id: uuid('id')
+    .primaryKey()
+    .default(sql`gen_random_uuid()`),
+  projectId: uuid('project_id')
+    .notNull()
+    .references(() => projectTable.id, { onDelete: 'cascade' }),
+  title: text('title'),
+  stacks: text('stacks').array(),
+  createdAt: text('created_at')
+    .notNull()
+    .$default(() => sql`NOW()`),
+  updatedAt: text('updated_at').$onUpdate(() => sql`NOW()`),
 });
