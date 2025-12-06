843f30f50afba8bbecea690c68c78b7008e3423c
ppyom <ouob77@kakao.com>
2025-12-06 18:09:36 +0900

    ✨ 로그인 페이지 구현

---
+++ app/login/page.tsx
@@ -0,0 +1,69 @@
+'use client';
+
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { signIn } from 'next-auth/react';
+import { isValidEmail } from '@/lib/utils';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import PageTitle from '@/components/page-title';
+
+export default function Page() {
+  const router = useRouter();
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = async () => {
+    if (!username || !password) {
+      return;
+    }
+
+    if (!isValidEmail(username)) {
+      return;
+    }
+
+    const response = await signIn('credentials', {
+      username,
+      password,
+      redirect: false,
+    });
+
+    if (!response) {
+      // 로그인 중 오류가 발생했습니다.
+      return;
+    }
+
+    if (response.error) {
+      // 아이디 또는 비밀번호를 확인해주세요.
+      return;
+    }
+
+    router.replace('/');
+  };
+
+  return (
+    <div className="max-w-md mx-auto flex flex-col justify-center gap-8 px-4 py-40">
+      <PageTitle>로그인</PageTitle>
+      <div className="space-y-4">
+        <Input
+          type="text"
+          placeholder="이메일 주소"
+          value={username}
+          onChange={({ target }) => setUsername(target.value)}
+        />
+        <Input
+          type="password"
+          placeholder="비밀번호"
+          value={password}
+          onChange={({ target }) => setPassword(target.value)}
+        />
+        <Button
+          className="w-full cursor-pointer font-semibold"
+          onClick={handleLogin}
+        >
+          로그인
+        </Button>
+      </div>
+    </div>
+  );
+}
--- lib/utils.ts
+++ lib/utils.ts
@@ -18,3 +18,11 @@ export function dynamicTextColor(hex: string) {
 
   return brightness < 186 ? 'white' : 'black';
 }
+
+/**
+ * 이메일주소 유효한 형식인지 검사하는 함수
+ * @param email 이메일주소
+ */
+export function isValidEmail(email: string): boolean {
+  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+}
