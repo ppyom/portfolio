0ca3165fe33736070797c3eeb97a4f8ddec904c4
ppyom <ouob77@kakao.com>
2025-12-07 01:34:49 +0900

    🦺 프로젝트 요청 데이터 검증을 위한 zod 스키마 추가

---
+++ lib/validation/project.schema.ts
@@ -0,0 +1,29 @@
+import { z } from 'zod';
+
+export const projectSchema = z.object({
+  title: z.string().nonempty(),
+  description: z.string().optional(),
+  category: z.string().optional(),
+  coverImageUrl: z.string().optional(),
+  githubUrl: z.string().optional(),
+  applicationUrl: z.string().optional(),
+  tags: z.array(z.string()),
+  overview: z.string(),
+  features: z.array(z.string()).optional(),
+  images: z.array(z.string()).optional(),
+  goals: z.array(z.string()).optional(),
+  results: z.array(z.string()).optional(),
+  member: z.object({
+    size: z.number(),
+    role: z.string(),
+    responsibilities: z.array(z.string()),
+  }),
+  techStacks: z.array(
+    z.object({
+      title: z.string(),
+      stacks: z.array(z.string()),
+    }),
+  ),
+});
+
+export type ProjectSchemaType = z.infer<typeof projectSchema>;
