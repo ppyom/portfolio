71635b6c20ad65ffe514a67c820274d9fd299daf
ppyom <ouob77@kakao.com>
2025-12-06 20:33:41 +0900

    ✨ 관리자 페이지의 header 컴포넌트 구현

---
+++ components/admin/admin-header.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+import { useMemo } from 'react';
+import { usePathname } from 'next/navigation';
+import { Separator } from '@/components/ui/separator';
+import { SidebarTrigger } from '@/components/ui/sidebar';
+import { sidebarItems } from '@/lib/constants/admin-sidebar-items';
+
+export default function AdminHeader() {
+  const pathname = usePathname();
+  const pageName = useMemo(
+    () => sidebarItems.find((item) => item.href === pathname)?.label || '',
+    [pathname],
+  );
+
+  return (
+    <header className="flex h-(--header-height) shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
+      <div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
+        <SidebarTrigger className="-ml-1" />
+        <Separator
+          orientation="vertical"
+          className="mx-2 data-[orientation=vertical]:h-4"
+        />
+        <h1 className="text-base font-medium">{pageName}</h1>
+      </div>
+    </header>
+  );
+}
