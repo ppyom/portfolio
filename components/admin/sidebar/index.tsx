c8159b4399647849d241db0953edc0fdc95fe521
ppyom <ouob77@kakao.com>
2025-12-06 20:23:35 +0900

    ✨ 관리자 페이지의 sidebar 컴포넌트 구현

---
+++ components/admin/sidebar/index.tsx
@@ -0,0 +1,66 @@
+import Link from 'next/link';
+import { getServerSession } from 'next-auth';
+import authOptions from '@/lib/auth-options';
+import { sidebarItems } from '@/lib/constants/admin-sidebar-items';
+import {
+  SidebarHeader,
+  Sidebar as SidebarRoot,
+  SidebarFooter,
+  SidebarContent,
+  SidebarMenuItem,
+  SidebarMenuButton,
+  SidebarMenu,
+  SidebarGroup,
+  SidebarGroupContent,
+} from '@/components/ui/sidebar';
+import { Separator } from '@/components/ui/separator';
+import LoginUser from '@/components/admin/sidebar/login-user';
+
+export default async function Sidebar() {
+  const session = await getServerSession(authOptions);
+
+  return (
+    <SidebarRoot>
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton asChild>
+              <Link href="/manage">
+                <p className="text-chart-2 text-xl font-bold font-dongle tracking-tighter">
+                  포트폴리오 관리자
+                </p>
+              </Link>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+
+      <Separator />
+
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {sidebarItems.map((item) => (
+                <SidebarMenuItem key={item.label}>
+                  <SidebarMenuButton tooltip={item.label} asChild>
+                    <Link href={item.href}>
+                      {item.icon && <item.icon />}
+                      <span>{item.label}</span>
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+
+      <Separator />
+
+      <SidebarFooter>
+        <LoginUser session={session} />
+      </SidebarFooter>
+    </SidebarRoot>
+  );
+}
---
+++ lib/constants/admin-sidebar-items.ts
@@ -0,0 +1,8 @@
+import { BookIcon, HomeIcon, StarIcon, UserIcon } from 'lucide-react';
+
+export const sidebarItems = [
+  { label: '홈', href: '/manage', icon: HomeIcon },
+  { label: '자기소개 관리', href: '#', icon: UserIcon },
+  { label: '보유 스킬 관리', href: '#', icon: StarIcon },
+  { label: '포트폴리오 관리', href: '#', icon: BookIcon },
+];
