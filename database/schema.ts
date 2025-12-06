aa1651f8a2af638ca638faac6d0655dfbda7cc97
ppyom <ouob77@kakao.com>
2025-12-06 14:20:56 +0900

    🗃️ User schema id 기본 값을 처리하도록 수정

--- database/schema.ts
+++ database/schema.ts
@@ -1,7 +1,10 @@
+import { sql } from 'drizzle-orm';
 import { pgTable, boolean, text, uuid } from 'drizzle-orm/pg-core';
 
 export const userTable = pgTable('user', {
-  id: uuid('id').primaryKey(),
+  id: uuid('id')
+    .primaryKey()
+    .default(sql`gen_random_uuid()`),
   username: text('username').notNull(),
   password: text('password').notNull(),
   name: text('name'),
