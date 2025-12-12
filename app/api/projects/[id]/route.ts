f34fa9c5fdbb3d6da7b57bf14e330eb259f69127
ppyom <ouob77@kakao.com>
2025-12-12 18:12:13 +0900

    🐛 프로젝트 업로드 중 발생한 오류 수정
    - 커버 이미지 업로드 중 existedCoverImage?.[0].deleted로 undefined.deleted로 적용되어 발생한 오류 수정
    - 이미지 파일 삭제 로직을 transaction.finally에 넣어서 발생한 오류 수정

--- app/api/projects/[id]/route.ts
+++ app/api/projects/[id]/route.ts
@@ -47,78 +47,77 @@ export const PATCH = async (request: Request, { params }: Payload) => {
   );
 
   try {
-    const result = await db
-      .transaction(async (tx) => {
-        // 0. 이미지파일 업데이트 (삭제할 파일들 삭제)
-        await tx.delete(fileTable).where(
-          inArray(
-            fileTable.id,
-            deletedImages.map((i) => i.id),
-          ),
-        );
-        const remainingImageIds = existedImages
-          .filter((i) => !i.deleted)
-          .map((i) => i.id);
+    const result = await db.transaction(async (tx) => {
+      // 0. 이미지파일 업데이트 (삭제할 파일들 삭제)
+      await tx.delete(fileTable).where(
+        inArray(
+          fileTable.id,
+          deletedImages.map((i) => i.id),
+        ),
+      );
+      const remainingImageIds = existedImages
+        .filter((i) => !i.deleted)
+        .map((i) => i.id);
 
-        // 1. 이미지 업로드
-        const coverImage = await uploadImage(coverImageFile, tx);
-        const images = (await uploadImage(imageFiles, tx)) || [];
+      // 1. 이미지 업로드
+      const coverImage = await uploadImage(coverImageFile, tx);
+      const images = (await uploadImage(imageFiles, tx)) || [];
 
-        // 2. 작성
-        const updated = await tx
-          .update(projectTable)
-          .set({
-            title,
-            description,
-            category,
-            githubUrl,
-            applicationUrl,
-            tags,
-            overview,
-            features,
-            goals,
-            results,
-            member,
-            coverImageId: !existedCoverImage?.[0].deleted
+      // 2. 작성
+      const updated = await tx
+        .update(projectTable)
+        .set({
+          title,
+          description,
+          category,
+          githubUrl,
+          applicationUrl,
+          tags,
+          overview,
+          features,
+          goals,
+          results,
+          member,
+          coverImageId:
+            existedCoverImage?.[0]?.deleted === false
               ? undefined
-              : coverImage?.[0].id || null,
-            imageIds: [...remainingImageIds, ...images.map((i) => i.id)],
-          })
-          .where(eq(projectTable.id, id))
-          .returning();
-        const project = updated[0];
+              : coverImage?.[0]?.id || null,
+          imageIds: [...remainingImageIds, ...images.map((i) => i.id)],
+        })
+        .where(eq(projectTable.id, id))
+        .returning();
+      const project = updated[0];
 
-        if (!project) {
+      if (!project) {
+        throw new Error('실패했습니다.');
+      }
+
+      if (techStacks.length > 0) {
+        const insertedTechStacks = await tx
+          .insert(projectTechStackTable)
+          .values(techStacks.map((t) => ({ ...t, projectId: id })))
+          .onConflictDoUpdate({
+            target: [
+              projectTechStackTable.projectId,
+              projectTechStackTable.title,
+            ],
+            set: {
+              stacks: sql`excluded.stacks`,
+            },
+          });
+        if (!insertedTechStacks) {
           throw new Error('실패했습니다.');
         }
+      }
 
-        if (techStacks.length > 0) {
-          const insertedTechStacks = await tx
-            .insert(projectTechStackTable)
-            .values(techStacks.map((t) => ({ ...t, projectId: id })))
-            .onConflictDoUpdate({
-              target: [
-                projectTechStackTable.projectId,
-                projectTechStackTable.title,
-              ],
-              set: {
-                stacks: sql`excluded.stacks`,
-              },
-            });
-          if (!insertedTechStacks) {
-            throw new Error('실패했습니다.');
-          }
-        }
+      return project;
+    });
 
-        return project;
-      })
-      .finally(async () => {
-        if (deletedImages.length === 0) return;
-
-        await Promise.all(
-          deletedImages.map(async (image) => await remove(image.url)),
-        );
-      });
+    if (deletedImages.length !== 0) {
+      await Promise.all(
+        deletedImages.map(async (image) => await remove(image.url)),
+      );
+    }
 
     return Response.json(result, { status: 200 });
   } catch (error) {
