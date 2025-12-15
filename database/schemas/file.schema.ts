193379263cd8d347cc106085650cf26812ac251d
ppyom <ouob77@kakao.com>
2025-12-15 21:59:24 +0900

    ♻️ schema 파일 분리

---
+++ database/schemas/file.schema.ts
@@ -0,0 +1,13 @@
+import { sql } from 'drizzle-orm';
+import { pgTable, text, uuid } from 'drizzle-orm/pg-core';
+
+export const fileTable = pgTable('file', {
+  id: uuid('id')
+    .primaryKey()
+    .default(sql`gen_random_uuid()`),
+  url: text('url').notNull(),
+  createdAt: text('created_at')
+    .notNull()
+    .$default(() => sql`NOW()`),
+  updatedAt: text('updated_at').$onUpdate(() => sql`NOW()`),
+});
---
+++ database/schemas/tech-stack.schema.ts
@@ -0,0 +1,28 @@
+import { pgTable, text, uuid } from 'drizzle-orm/pg-core';
+import { sql } from 'drizzle-orm';
+import { unique } from 'drizzle-orm/pg-core/unique-constraint';
+import { projectTable } from './project.schema';
+
+export const projectTechStackTable = pgTable(
+  'project_tech_stack',
+  {
+    id: uuid('id')
+      .primaryKey()
+      .default(sql`gen_random_uuid()`),
+    projectId: uuid('project_id')
+      .notNull()
+      .references(() => projectTable.id, { onDelete: 'cascade' }),
+    title: text('title'),
+    stacks: text('stacks').array(),
+    createdAt: text('created_at')
+      .notNull()
+      .$default(() => sql`NOW()`),
+    updatedAt: text('updated_at').$onUpdate(() => sql`NOW()`),
+  },
+  (table) => ({
+    project_tech_stack_projectId_title_unique: unique().on(
+      table.projectId,
+      table.title,
+    ),
+  }),
+);
