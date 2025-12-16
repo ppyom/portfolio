a41f85a0616f16afec004a170f85cda0f66e0910
ppyom <ouob77@kakao.com>
2025-12-16 22:18:36 +0900

    🏷️ Profile 타입 추가

--- database/types.d.ts
+++ database/types.d.ts
@@ -1,4 +1,11 @@
-import { fileTable, projectTable, techStackTable } from '@/database/schema';
+import {
+  educationTable,
+  experienceTable,
+  fileTable,
+  profileTable,
+  projectTable,
+  techStackTable,
+} from '@/database/schema';
 
 export namespace FileTable {
   type Select = typeof fileTable.$inferSelect;
@@ -13,4 +20,19 @@ export namespace ProjectTable {
 export namespace TechStackTable {
   type Select = typeof techStackTable.$inferSelect;
   type Insert = typeof techStackTable.$inferInsert;
+}
+
+export namespace ProfileTable {
+  type Select = typeof profileTable.$inferSelect;
+  type Insert = typeof profileTable.$inferInsert;
+}
+
+export namespace ExperienceTable {
+  type Select = typeof experienceTable.$inferSelect;
+  type Insert = typeof experienceTable.$inferInsert;
+}
+
+export namespace EducationTable {
+  type Select = typeof educationTable.$inferSelect;
+  type Insert = typeof educationTable.$inferInsert;
 }
