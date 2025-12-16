df73cbcb732e7a5c0973ab0eb463d27916217c4e
ppyom <ouob77@kakao.com>
2025-12-16 21:53:37 +0900

    🗃️ 프로필 조회 쿼리 작성

---
+++ database/queries/profile.ts
@@ -0,0 +1,69 @@
+import { eq, sql } from 'drizzle-orm';
+import { db } from '@/database';
+import { profileTable } from '@/database/schemas/profile.schema';
+import { experienceTable } from '@/database/schemas/experience.schema';
+import { educationTable } from '@/database/schemas/education.schema';
+
+const experienceSubQuery = db
+  .select({
+    profileId: experienceTable.profileId,
+    experience: sql`
+      COALESCE(
+        JSON_AGG(
+          JSON_BUILD_OBJECT(
+            'id', ${experienceTable.id},
+            'name', ${experienceTable.name},
+            'position', ${experienceTable.position},
+            'startDate', ${experienceTable.startDate},
+            'endDate', ${experienceTable.endDate}
+          )
+        ),
+        '[]'
+      )
+    `.as('experience'),
+  })
+  .from(experienceTable)
+  .groupBy(experienceTable.profileId)
+  .as('experience');
+
+const educationSubQuery = db
+  .select({
+    profileId: educationTable.profileId,
+    education: sql`
+      COALESCE(
+        JSON_AGG(
+          JSON_BUILD_OBJECT(
+            'id', ${educationTable.id},
+            'name', ${educationTable.name},
+            'major', ${educationTable.major},
+            'startDate', ${educationTable.startDate},
+            'endDate', ${educationTable.endDate}
+          )
+        ),
+        '[]'
+      )
+    `.as('education'),
+  })
+  .from(educationTable)
+  .groupBy(educationTable.profileId)
+  .as('education');
+
+const baseQuery = db
+  .select({
+    introduce: profileTable.introduce,
+    experience: experienceSubQuery.experience,
+    education: educationSubQuery.education,
+  })
+  .from(profileTable)
+  .leftJoin(
+    experienceSubQuery,
+    eq(profileTable.id, experienceSubQuery.profileId),
+  )
+  .leftJoin(
+    educationSubQuery,
+    eq(profileTable.id, educationSubQuery.profileId),
+  );
+
+export const getProfile = baseQuery
+  .where(eq(profileTable.language, sql.placeholder('language')))
+  .prepare('get_profile');
