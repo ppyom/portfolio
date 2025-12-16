7f813c1873ffe7ed42e9d9b66a4855185772b1ad
ppyom <ouob77@kakao.com>
2025-12-16 21:27:25 +0900

    🗃️ 프로필 스키마 추가 / Drizzle 마이그레이션

---
+++ database/schemas/profile.schema.ts
@@ -0,0 +1,14 @@
+import { sql } from 'drizzle-orm';
+import { pgTable, text, uuid } from 'drizzle-orm/pg-core';
+
+export const profileTable = pgTable('profile', {
+  id: uuid('id')
+    .primaryKey()
+    .default(sql`gen_random_uuid()`),
+  language: text('language').notNull().default('ko'),
+  introduce: text('introduce').array(),
+  createdAt: text('created_at')
+    .notNull()
+    .$default(() => sql`NOW()`),
+  updatedAt: text('updated_at').$onUpdate(() => sql`NOW()`),
+});
