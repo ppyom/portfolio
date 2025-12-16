6e6d9b47ce968d245d4978ae8aa19ef6243019b4
ppyom <ouob77@kakao.com>
2025-12-17 00:01:49 +0900

    ♻️ ArrayField 컴포넌트에 Textarea를 지원하도록 수정

--- components/form/array-field.tsx
+++ components/form/array-field.tsx
@@ -4,19 +4,28 @@ import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
 import { PlusIcon, Trash2Icon } from 'lucide-react';
 import { useStringArray } from '@/hooks/use-string-array';
+import { Textarea } from '@/components/ui/textarea';
 
 interface Props {
   name: string;
+  textarea?: boolean;
   placeholder?: string;
 }
 
-export default function ArrayField({ name, placeholder = '' }: Props) {
+export default function ArrayField({
+  name,
+  textarea,
+  placeholder = '',
+}: Props) {
   const { fields, update, append, remove } = useStringArray(name);
+  const InputComponent = textarea ? Textarea : Input;
+
   return (
     <>
       {fields.map((field, idx) => (
         <div key={`${name}_${idx}`} className="flex items-center gap-2">
-          <Input
+          <InputComponent
+            className="resize-none"
             placeholder={placeholder}
             value={field}
             onChange={({ target }) => update(idx, target.value)}
