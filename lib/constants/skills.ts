4cd114e05f2c21bd1ce3a3ce2e1b368c32f09c1f
ppyom <ouob77@kakao.com>
2025-12-25 15:43:04 +0900

    ♻️ SkillTag에 컬러 매핑 적용

--- lib/constants/skills.ts
+++ lib/constants/skills.ts
@@ -1,3 +1,6 @@
+/**
+ * @deprecated
+ */
 export const skills = [
   {
     category: 'Frontend',
@@ -16,16 +19,3 @@ export const skills = [
     items: ['Git'],
   },
 ];
-
-export const skillHex: Record<string, string> = {
-  React: '#61DAFB',
-  'Next.js': '#000000',
-  TypeScript: '#3178C6',
-  'Tailwind CSS': '#06B6D4',
-  'Node.js': '#5FA04E',
-  'Nest.js': '#E0234E',
-  PostgreSQL: '#4169E1',
-  Figma: '#F24E1E',
-  Storybook: '#FF4785',
-  Git: '#F05032',
-};
