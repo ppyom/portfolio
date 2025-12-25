cbf0b4e33aa00094258f4ca8bf1b04de6cf672e0
ppyom <ouob77@kakao.com>
2025-12-25 15:49:48 +0900

    🏷️ skill 타입 정의

--- types/skill.d.ts
+++ types/skill.d.ts
@@ -1,3 +1,10 @@
+import { SkillCategoryTable, SkillTable } from '@/database/types/skill';
+
+export interface Skill {
+  category: SkillCategoryTable.Select['name'] | null;
+  items: SkillTable.Select['name'][];
+}
+
 export interface SkillMetadata {
   color: string;
 }
