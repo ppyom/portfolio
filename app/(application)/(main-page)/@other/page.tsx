93903ff307ee3dc0dc02379d0b9fa506912a918c
ppyom <ouob77@kakao.com>
2025-12-27 19:26:03 +0900

    ✨ Learning & Growth 섹션에 데이터 바인딩

--- app/(application)/(main-page)/@other/page.tsx
+++ app/(application)/(main-page)/@other/page.tsx
@@ -1,9 +1,20 @@
-import { certifications, learnings } from '@/lib/constants/achievements';
+import { eq } from 'drizzle-orm';
+
+import { db } from '@/database';
+import { historyTable } from '@/database/schema/history.schema';
 import PageTitle from '@/components/common/page-title';
 import SectionTitle from '@/components/common/section-title';
 import AchievementCard from '@/components/application/achievement-card';
 
-export default function Other() {
+export default async function Other() {
+  const learnings = await db
+    .select()
+    .from(historyTable)
+    .where(eq(historyTable.type, 'learning'));
+  const certifications = await db
+    .select()
+    .from(historyTable)
+    .where(eq(historyTable.type, 'certification'));
   return (
     <section id="other" className="py-20 px-6">
       <div className="max-w-4xl mx-auto space-y-12">
