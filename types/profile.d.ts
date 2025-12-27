8dfb212def5a923155fcfeab236be3b17c67ea8a
ppyom <ouob77@kakao.com>
2025-12-27 19:21:26 +0900

    ✨ 프로필 관리 폼에 활동 이력 필드 추가

--- types/profile.d.ts
+++ types/profile.d.ts
@@ -1,10 +1,12 @@
 import {
   EducationTable,
   ExperienceTable,
+  HistoryTable,
   ProfileTable,
 } from '@/database/types/profile';
 
 export interface Profile extends ProfileTable.Select {
   experience: ExperienceTable.Select[];
   education: EducationTable.Select[];
+  history: HistoryTable.Select[];
 }
