bfe04eed1fd1e10755f6a7093fe8e771298d3c4a
ppyom <ouob77@kakao.com>
2026-01-01 19:27:08 +0900

    ♻️ contact 조회 함수 이름 수정

--- database/queries/contact.ts
+++ database/queries/contact.ts
@@ -3,10 +3,10 @@ import { desc, eq } from 'drizzle-orm';
 import { db } from '@/database';
 import { contactTable } from '@/database/schema/contact.schema';
 
-export const getContacts = () =>
+export const getInboxMessages = () =>
   db.select().from(contactTable).orderBy(desc(contactTable.createdAt));
 
-export const getContact = (id: string) =>
+export const getInboxMessage = (id: string) =>
   db
     .select()
     .from(contactTable)
