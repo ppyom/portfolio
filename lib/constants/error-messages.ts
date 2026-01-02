4049a69b49de7c5cc2360b9c85e9246f2f8dd2a1
ppyom <ouob77@kakao.com>
2026-01-02 18:09:42 +0900

    ♻️ Profile 폼 Zod 스키마 일관화 적용

--- lib/constants/error-messages.ts
+++ lib/constants/error-messages.ts
@@ -6,6 +6,22 @@ export const errorMessages = {
     email: '이메일은 필수 입력 항목입니다.',
     title: '제목은 필수 입력 항목입니다.',
     content: '내용은 필수 입력 항목입니다.',
+    profile: {
+      experience: {
+        position: '직무는 필수 입력 항목입니다.',
+        startDate: '입사일은 필수 입력 항목입니다.',
+      },
+      education: {
+        name: '학교 이름은 필수 입력 항목입니다.',
+        major: '전공은 필수 입력 항목입니다.',
+        startDate: '입학일은 필수 입력 항목입니다.',
+      },
+      history: {
+        type: '유형은 필수 선택 항목입니다.',
+        name: '제목은 필수 입력 항목입니다.',
+        date: '날짜는 필수 입력 항목입니다.',
+      },
+    },
   },
   invalid: {
     email: '이메일 주소 형식이 올바르지 않습니다.',
--- lib/validation/profile.schema.ts
+++ lib/validation/profile.schema.ts
@@ -1,29 +1,38 @@
 import { z } from 'zod';
 
-export const profileSchema = z.object({
+import { errorMessages } from '@/lib/constants/error-messages';
+
+const { profile } = errorMessages.required;
+
+export const schema = z.object({
   introduce: z.string().array(),
   experience: z
     .object({
       name: z.string(),
-      position: z.string(),
-      startDate: z.string(),
+      position: z.string().nonempty(profile.experience.position),
+      startDate: z.string().nonempty(profile.experience.startDate),
       endDate: z.string(),
     })
     .array(),
   education: z
     .object({
-      name: z.string(),
-      major: z.string(),
-      startDate: z.string(),
+      name: z.string().nonempty(profile.education.name),
+      major: z.string().nonempty(profile.education.major),
+      startDate: z.string().nonempty(profile.education.startDate),
       endDate: z.string(),
     })
     .array(),
   history: z
     .object({
-      type: z.enum(['learning', 'certification', 'activity']),
-      name: z.string(),
-      date: z.string(),
+      type: z.enum(
+        ['learning', 'certification', 'activity'],
+        profile.history.type,
+      ),
+      date: z.string().nonempty(profile.history.date),
+      name: z.string().nonempty(profile.history.name),
       description: z.string(),
     })
     .array(),
 });
+
+export type FormDataType = z.infer<typeof schema>;
