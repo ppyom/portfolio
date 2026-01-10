26511a98280b1bc48a7f621be72b76800ae7ad5e
ppyom <ouob77@kakao.com>
2026-01-10 12:43:52 +0900

    ♻️ 관리자 프로젝트 리스트에 작성되던 프로젝트 카드 분리

---
+++ components/admin/projects/project-card.tsx
@@ -0,0 +1,79 @@
+import Link from 'next/link';
+import { ArrowUpRightIcon } from 'lucide-react';
+
+import { fullDateString } from '@/lib/utils/date';
+import { Button } from '@/components/ui/button';
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import SkillTag from '@/components/common/skill-tag';
+import type { Project } from '@/types/project';
+
+import ProjectDropdown from './project-dropdown';
+import ProjectVisibilityToggle from './project-visibility-toggle';
+
+interface Props {
+  project: Project;
+}
+
+export default function ProjectCard({ project }: Props) {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle className="flex gap-2 items-center">
+          {project.title}
+          {project.isPublic && (
+            <Button
+              variant="ghost"
+              size="sm"
+              className="text-xs text-muted-foreground"
+              asChild
+            >
+              <Link
+                href={`/projects/${project.id}`}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <span className="hidden sm:inline">프로젝트 보기</span>
+                <ArrowUpRightIcon className="size-3" />
+              </Link>
+            </Button>
+          )}
+        </CardTitle>
+        <CardDescription>{project.description}</CardDescription>
+        <CardAction className="space-x-1">
+          <ProjectVisibilityToggle
+            projectId={project.id}
+            isPublic={!!project.isPublic}
+          />
+          <ProjectDropdown projectId={project.id} />
+        </CardAction>
+      </CardHeader>
+      <CardContent className="space-y-2">
+        <div className="flex items-center justify-between">
+          <span className="text-muted-foreground">마지막 수정 일자</span>
+          <span className="font-semibold text-sm">
+            {fullDateString(project.updatedAt || project.createdAt)}
+          </span>
+        </div>
+        <div className="flex items-center justify-between">
+          <span className="text-muted-foreground">태그</span>
+          <div className="inline-flex gap-2 flex-wrap">
+            {project.tags?.slice(0, 5).map((tag) => (
+              <SkillTag
+                key={`skill_tag-${project.id}-${tag}`}
+                name={tag}
+                size="sm"
+              />
+            ))}
+          </div>
+        </div>
+      </CardContent>
+    </Card>
+  );
+}
--- components/admin/projects/project-list.tsx
+++ components/admin/projects/project-list.tsx
@@ -1,22 +1,6 @@
-import Link from 'next/link';
-import { ArrowUpRightIcon } from 'lucide-react';
-
-import { cn } from '@/lib/utils';
-import { fullDateString } from '@/lib/utils/date';
-import { Button } from '@/components/ui/button';
-import {
-  Card,
-  CardAction,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card';
-import SkillTag from '@/components/common/skill-tag';
 import type { Project } from '@/types/project';
 
-import ProjectDropdown from './project-dropdown';
-import ProjectVisibilityToggle from './project-visibility-toggle';
+import ProjectCard from './project-card';
 
 interface Props {
   projects: Project[];
@@ -27,58 +11,7 @@ export default function ProjectList({ projects }: Props) {
     <ul className="space-y-4">
       {projects.map((project) => (
         <li key={project.id}>
-          <Card>
-            <CardHeader>
-              <CardTitle className="flex gap-2 items-center">
-                {project.title}
-                {project.isPublic && (
-                  <Button
-                    variant="ghost"
-                    size="sm"
-                    className="text-xs text-muted-foreground"
-                    asChild
-                  >
-                    <Link
-                      href={`/projects/${project.id}`}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <span className="hidden sm:inline">프로젝트 보기</span>
-                      <ArrowUpRightIcon className="size-3" />
-                    </Link>
-                  </Button>
-                )}
-              </CardTitle>
-              <CardDescription>{project.description}</CardDescription>
-              <CardAction className="space-x-1">
-                <ProjectVisibilityToggle
-                  projectId={project.id}
-                  isPublic={!!project.isPublic}
-                />
-                <ProjectDropdown projectId={project.id} />
-              </CardAction>
-            </CardHeader>
-            <CardContent className="space-y-2">
-              <div className="flex items-center justify-between">
-                <span className="text-muted-foreground">마지막 수정 일자</span>
-                <span className="font-semibold text-sm">
-                  {fullDateString(project.updatedAt || project.createdAt)}
-                </span>
-              </div>
-              <div className="flex items-center justify-between">
-                <span className="text-muted-foreground">태그</span>
-                <div className="inline-flex gap-2 flex-wrap">
-                  {project.tags?.slice(0, 5).map((tag) => (
-                    <SkillTag
-                      key={`skill_tag-${project.id}-${tag}`}
-                      name={tag}
-                      size="sm"
-                    />
-                  ))}
-                </div>
-              </div>
-            </CardContent>
-          </Card>
+          <ProjectCard project={project} />
         </li>
       ))}
     </ul>
