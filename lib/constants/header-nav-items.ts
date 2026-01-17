a5b9833b7cd048f02c8ef24bdc8f963a427586b6
ppyom <ouob77@kakao.com>
2026-01-17 22:26:38 +0900

    ✨ About 페이지 구현

--- lib/constants/header-nav-items.ts
+++ lib/constants/header-nav-items.ts
@@ -1,7 +1,7 @@
 import type { NavItem } from '@/types/nav-item';
 
 export const headerNavItems: NavItem[] = [
-  { label: 'About', href: '#about', type: 'anchor' },
+  { label: 'About', href: '/about', type: 'page' },
   { label: 'Projects', href: '/projects', type: 'page' },
   { label: 'Contact', href: '/contact', type: 'page' },
 ];
