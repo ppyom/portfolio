e1e9a5f662c7e19203450f0a87c2ee2c350a0b37
ppyom <ouob77@kakao.com>
2026-06-05 17:49:00 +0900

    🩹 오타 수정

--- app/manage/profile/page.tsx
+++ app/manage/profile/page.tsx
@@ -13,7 +13,7 @@ export default async function Page() {
   return (
     <div className="mx-auto w-full max-w-5xl px-6 py-8 md:px-8 space-y-8">
       <div className="flex-1 space-y-2">
-        <p className="text-2xl font-bold">프로젝트 관리</p>
+        <p className="text-2xl font-bold">프로필 관리</p>
         <p className="text-text-secondary">
           자기소개, 경력, 학력 및 이력 정보를 관리하고 표시 순서를 조정할 수
           있습니다.
