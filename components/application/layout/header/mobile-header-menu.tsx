857312ac5665bdc60ab7147a40d4a8adaf33c0cc
ppyom <ouob77@kakao.com>
2026-06-16 17:50:31 +0900

    ♻️ MobileHeaderMenu 컴포넌트 리팩토링

--- components/application/layout/header/mobile-header-menu.tsx
+++ components/application/layout/header/mobile-header-menu.tsx
@@ -4,16 +4,13 @@ import { usePathname } from 'next/navigation';
 import type { Session } from 'next-auth';
 import { MenuIcon, XIcon } from 'lucide-react';
 
-import { Button } from '@/components/ui-legacy/button';
+import { Button } from '@/components/ui/button';
 import {
   Sheet,
   SheetClose,
   SheetContent,
-  SheetDescription,
-  SheetHeader,
-  SheetTitle,
   SheetTrigger,
-} from '@/components/ui-legacy/sheet';
+} from '@/components/ui/sheet';
 import { ThemeToggle } from '@/components/common/theme/theme-toggle';
 
 import { HeaderNavigation } from './header-navigation';
@@ -28,31 +25,27 @@ export function MobileHeaderMenu({ session }: Props) {
 
   return (
     <Sheet key={pathname}>
-      <SheetTrigger asChild>
-        <Button variant="ghost" size="icon-sm">
-          <MenuIcon />
+      <SheetTrigger>
+        <Button variant="ghost" size="sm">
+          <MenuIcon size={14} />
         </Button>
       </SheetTrigger>
 
       <SheetContent
         side="right"
-        className="flex flex-col gap-6 [&>button]:hidden"
+        className="flex flex-col p-4 gap-6 [&>button]:hidden"
       >
-        <SheetHeader>
-          <SheetTitle hidden />
-          <SheetDescription hidden />
-          <div className="flex gap-2 items-center justify-between">
-            <div className="flex-1">
-              <UserMenu session={session} />
-            </div>
-            <ThemeToggle />
-            <SheetClose asChild>
-              <Button variant="ghost" size="icon-sm">
-                <XIcon />
-              </Button>
-            </SheetClose>
+        <div className="flex gap-2 items-center justify-between">
+          <div className="flex-1 ml-2">
+            <UserMenu session={session} />
           </div>
-        </SheetHeader>
+          <ThemeToggle />
+          <SheetClose>
+            <Button className="text-text-muted" variant="ghost" size="sm">
+              <XIcon size={14} />
+            </Button>
+          </SheetClose>
+        </div>
         <nav className="flex flex-col items-center gap-6 text-sm font-medium">
           <HeaderNavigation />
         </nav>
