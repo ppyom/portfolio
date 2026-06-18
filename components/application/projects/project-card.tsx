cb3b77223e6fe0ee7118a0918743d7e0c01d59b7
ppyom <ouob77@kakao.com>
2026-06-18 22:42:35 +0900

    ♻️ ProjectCard 컴포넌트 리팩토링
    - category 표시 추가
    - title, description에 line-clamp 추가
    - 전체적인 디자인 다듬기

--- components/application/projects/project-card.tsx
+++ components/application/projects/project-card.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
+import { Badge } from '@/components/ui/badge';
 import { SkillTag } from '@/components/application/skills/skill-tag';
 import type { Project } from '@/types/project';
 
@@ -8,35 +9,41 @@ type Props = Project;
 
 export function ProjectCard({
   id,
+  category,
   coverImage: image,
   title,
   description,
   tags,
 }: Props) {
   return (
-    <Link href={`/projects/${id}`} scroll={false}>
-      <div className="group bg-background rounded-xl overflow-hidden border border-border glow-hover">
-        <div className="overflow-hidden h-48 bg-muted">
+    <Link href={`/projects/${id}`} scroll={false} className="block">
+      <div className="group bg-surface-secondary rounded-md overflow-hidden border">
+        <div className="relative overflow-hidden h-48 bg-text-muted">
           <Image
             src={image?.url || '/images/no_image.png'}
             alt={title}
-            className="w-full h-full object-cover group-hover:scale-105 transition-transform duration-300"
+            className="size-full object-cover group-hover:scale-105 transition-transform duration-300"
             width={500}
             height={500}
             loading="eager"
           />
+          {category && (
+            <Badge className="absolute top-4 left-4" variant="secondary">
+              {category}
+            </Badge>
+          )}
         </div>
-        <div className="p-6 space-y-4">
-          <p className="text-xl font-bold text-foreground group-hover:text-primary transition-colors duration-200">
+        <div className="p-6">
+          <p className="text-lg font-bold text-text-primary line-clamp-1">
             {title}
           </p>
-          <p className="text-foreground/70 text-sm leading-relaxed">
+          <p className="text-text-secondary leading-relaxed line-clamp-2">
             {description}
           </p>
           <div className="flex flex-wrap gap-2 pt-2">
-            {tags &&
-              tags.length !== 0 &&
-              tags.map((tag) => <SkillTag key={tag} name={tag} size="sm" />)}
+            {tags?.map((tag) => (
+              <SkillTag key={tag} name={tag} size="sm" />
+            ))}
           </div>
         </div>
       </div>
