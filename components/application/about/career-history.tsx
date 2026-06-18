0ebf8ae4a1c194901aada199dd39588ef384e51d
ppyom <ouob77@kakao.com>
2026-06-18 16:33:53 +0900

    ♻️ CareerHistory 수정
    - 없는 경우 렌더링 X

--- components/application/about/career-history.tsx
+++ components/application/about/career-history.tsx
@@ -6,43 +6,51 @@ interface Props {
 }
 
 export function CareerHistory({ experiences, educations }: Props) {
+  if (!experiences && !educations) {
+    return null;
+  }
+
   return (
     <>
-      <div className="bg-surface-secondary rounded-md p-6 border">
-        <p className="text-text-primary font-bold text-lg mb-2">Experience</p>
-        <ul className="space-y-1 text-text-secondary">
-          {experiences?.map((content, idx) => (
-            <li key={`experience_${idx}`}>
-              {content.name && (
-                <>
-                  <span className="font-semibold">{content.name}</span>
-                  <span className="text-sm"> / {content.position} </span>
-                </>
-              )}
-              {!content.name && (
-                <span className="font-semibold mr-2">{content.position}</span>
-              )}
-              <span className="text-sm">
-                ({content.startDate} - {content.endDate ?? '재직중'})
-              </span>
-            </li>
-          ))}
-        </ul>
-      </div>
-      <div className="bg-surface-secondary rounded-md p-6 border">
-        <p className="text-text-primary font-bold text-lg mb-2">Education</p>
-        <ul className="space-y-1 text-text-secondary">
-          {educations?.map((content, idx) => (
-            <li key={`education_${idx}`}>
-              <span className="font-semibold">{content.name}</span>
-              <span className="text-sm"> / {content.major} </span>
-              <span className="text-sm">
-                ({content.startDate} - {content.endDate ?? '재학중'})
-              </span>
-            </li>
-          ))}
-        </ul>
-      </div>
+      {experiences && (
+        <div className="bg-surface-secondary rounded-md p-6 border">
+          <p className="text-text-primary font-bold text-lg mb-2">Experience</p>
+          <ul className="space-y-1 text-text-secondary">
+            {experiences.map((content, idx) => (
+              <li key={`experience_${idx}`}>
+                {content.name && (
+                  <>
+                    <span className="font-semibold">{content.name}</span>
+                    <span className="text-sm"> / {content.position} </span>
+                  </>
+                )}
+                {!content.name && (
+                  <span className="font-semibold mr-2">{content.position}</span>
+                )}
+                <span className="text-sm">
+                  ({content.startDate} - {content.endDate ?? '재직중'})
+                </span>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+      {educations && (
+        <div className="bg-surface-secondary rounded-md p-6 border">
+          <p className="text-text-primary font-bold text-lg mb-2">Education</p>
+          <ul className="space-y-1 text-text-secondary">
+            {educations.map((content, idx) => (
+              <li key={`education_${idx}`}>
+                <span className="font-semibold">{content.name}</span>
+                <span className="text-sm"> / {content.major} </span>
+                <span className="text-sm">
+                  ({content.startDate} - {content.endDate ?? '재학중'})
+                </span>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </>
   );
 }
