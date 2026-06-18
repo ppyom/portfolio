062826b9e410fc9666421d2ac77b350304057c32
ppyom <ouob77@kakao.com>
2026-06-18 15:50:21 +0900

    💬 Mock 데이터 보강

--- mocks/profile.mock.ts
+++ mocks/profile.mock.ts
@@ -3,7 +3,11 @@ import type { Profile } from '@/types/profile';
 export const profileMock: Profile = {
   id: '1',
   language: 'ko',
-  introduce: ['PROFILE'],
+  introduce: [
+    'Aliquam ultrices, ante non imperdiet lacinia, diam tortor eleifend erat, eu dapibus tortor nibh id risus. Vivamus eleifend mauris eu bibendum laoreet. Curabitur laoreet, diam ac feugiat vehicula, mi ex dapibus leo, eu lacinia neque felis id augue. Sed interdum nulla lobortis quam tincidunt blandit. Suspendisse eu est erat.',
+    'Cras varius velit est. Mauris commodo vitae orci ac suscipit. Nulla a nulla suscipit, porta nisl ac, semper ante. Aenean eget diam vestibulum, accumsan lorem quis, bibendum turpis. Sed a pharetra tellus. Vivamus ut massa nulla. Phasellus bibendum sapien ac fermentum feugiat. Suspendisse vel leo molestie, iaculis velit a, molestie augue. ',
+    'Nullam dui arcu, venenatis id pellentesque vel, commodo id purus. Vestibulum lorem metus, venenatis eu velit a, tristique congue libero. Mauris cursus, ante eu accumsan pulvinar, nisi nulla pretium turpis, non hendrerit quam ligula ut enim. In quis metus faucibus, dictum quam ac, ultrices arcu. Phasellus finibus sodales convallis.',
+  ],
   experience: [
     {
       id: '1',
@@ -36,7 +40,30 @@ export const profileMock: Profile = {
       type: 'activity',
       name: 'Activity',
       date: '2026.05 - 2026.06',
-      description: 'Activity',
+      description:
+        'Curabitur laoreet, diam ac feugiat vehicula, mi ex dapibus leo, eu lacinia neque felis id augue. Sed interdum nulla lobortis quam tincidunt blandit.',
+      order: 0,
+      createdAt: new Date().toISOString(),
+      updatedAt: new Date().toISOString(),
+    },
+    {
+      id: '2',
+      profileId: '1',
+      type: 'learning',
+      name: 'Learning',
+      date: '2026.05 - 2026.06',
+      description: '',
+      order: 0,
+      createdAt: new Date().toISOString(),
+      updatedAt: new Date().toISOString(),
+    },
+    {
+      id: '3',
+      profileId: '1',
+      type: 'certification',
+      name: 'Certification',
+      date: '2026.05',
+      description: '',
       order: 0,
       createdAt: new Date().toISOString(),
       updatedAt: new Date().toISOString(),
