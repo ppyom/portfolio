ba712e014426a1eb5ec943e52890276089b677b8
ppyom <ouob77@kakao.com>
2026-06-19 22:21:29 +0900

    ♻️ Images 컴포넌트 리팩토링
    - legacy 제거 후 New Carousel 컴포넌트 적용
    - 이미지 개수가 1개 이상인 경우에만 loop, control 버튼 보이게 수정

--- components/application/projects/project-detail/images.tsx
+++ components/application/projects/project-detail/images.tsx
@@ -8,7 +8,7 @@ import {
   CarouselItem,
   CarouselNext,
   CarouselPrevious,
-} from '@/components/ui-legacy/carousel';
+} from '@/components/ui/carousel';
 import type { ImageFile } from '@/types/project';
 
 import { useImagePreview } from './image-preview';
@@ -21,30 +21,32 @@ export function Images({ images }: Props) {
   const { open } = useImagePreview();
 
   const imageUrls = images.map((image) => image.url);
+  const hasMultipleImages = images.length > 1;
 
   return (
-    <Carousel
-      opts={{
-        align: 'center',
-        loop: true,
-      }}
-    >
-      <CarouselContent>
-        {images.map((image, idx) => (
-          <CarouselItem key={image.id} className="basis-full sm:basis-1/2">
-            <Image
-              className="w-full aspect-square object-center object-cover"
-              src={image.url}
-              alt={`시연 이미지 ${idx}`}
-              width={500}
-              height={500}
-              onClick={() => open(imageUrls, idx)}
-            />
-          </CarouselItem>
-        ))}
-      </CarouselContent>
-      <CarouselPrevious />
-      <CarouselNext />
-    </Carousel>
+    <div className="relative">
+      <Carousel loop={hasMultipleImages}>
+        <CarouselContent>
+          {images.map((image, idx) => (
+            <CarouselItem key={image.id} className="basis-full sm:basis-1/2">
+              <Image
+                className="w-full aspect-square object-center object-cover cursor-pointer"
+                src={image.url}
+                alt={`시연 이미지 ${idx + 1}`}
+                width={500}
+                height={500}
+                onClick={() => open(imageUrls, idx)}
+              />
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        {hasMultipleImages && (
+          <div className="absolute top-1/2 left-2 right-2 -translate-y-1/2 flex justify-between">
+            <CarouselPrevious className="bg-surface-elevated shadow-lg" />
+            <CarouselNext className="bg-surface-elevated shadow-lg" />
+          </div>
+        )}
+      </Carousel>
+    </div>
   );
 }
