8b31b6f13a684ca014740ed97a3b938ebd9fb422
ppyom <ouob77@kakao.com>
2026-06-21 16:36:47 +0900

    ♻️ 프로젝트 상세 콘텐츠 컴포넌트 수정
    - 전체적인 spacing 수정
    - 순서 변경 (개요 -> 기능 -> 기술 -> 구성원 -> 목표/성과 -> 이미지)

--- components/application/projects/project-detail/project-detail.tsx
+++ components/application/projects/project-detail/project-detail.tsx
@@ -16,45 +16,45 @@ export function ProjectDetail({ project }: Props) {
   return (
     <ImagePreviewProvider>
       <Header {...project} />
-      <main className="max-w-4xl mx-auto p-6 space-y-6">
+      <main className="max-w-4xl mx-auto p-6 space-y-8">
         {project.overview && (
-          <Section>
+          <Section className="space-y-4">
             <SectionTitle>프로젝트 개요</SectionTitle>
             <p className="whitespace-pre-wrap">{project.overview}</p>
           </Section>
         )}
         {project.features && project.features.length !== 0 && (
-          <Section>
+          <Section className="space-y-4">
             <SectionTitle>주요 기능</SectionTitle>
             <BulletList items={project.features} />
           </Section>
         )}
-        {project.member && (
-          <Section>
-            <SectionTitle>프로젝트 구성원</SectionTitle>
-            <Member member={project.member} />
-          </Section>
-        )}
         {project.techStacks && project.techStacks.length !== 0 && (
-          <Section>
+          <Section className="space-y-4">
             <SectionTitle>사용 기술</SectionTitle>
             <TechStack techStacks={project.techStacks} />
           </Section>
         )}
+        {project.member && (
+          <Section className="space-y-4">
+            <SectionTitle>프로젝트 구성원</SectionTitle>
+            <Member member={project.member} />
+          </Section>
+        )}
         {project.goals && project.goals.length !== 0 && (
-          <Section>
+          <Section className="space-y-4">
             <SectionTitle>목표</SectionTitle>
             <BulletList items={project.goals} />
           </Section>
         )}
         {project.results && project.results.length !== 0 && (
-          <Section>
+          <Section className="space-y-4">
             <SectionTitle>성과</SectionTitle>
             <BulletList items={project.results} />
           </Section>
         )}
         {project.images && project.images.length !== 0 && (
-          <Section>
+          <Section className="space-y-4">
             <SectionTitle>시연 이미지</SectionTitle>
             <Images images={project.images} />
           </Section>
