d36d1b41c888874b260b94b23f7292bc940b851a
ppyom <ouob77@kakao.com>
2026-06-21 18:45:38 +0900

    ♻️ History 섹션 수정

--- app/(application)/(main-page)/@history/page.tsx
+++ app/(application)/(main-page)/@history/page.tsx
@@ -1,21 +1,16 @@
-import {
-  getCertificationsQuery,
-  getLearningsQuery,
-} from '@/database/queries/history';
+import { getHistoriesQuery } from '@/database/queries/history';
+import { Section, SectionTitle } from '@/components/ui/section';
 import { Achievement } from '@/components/application/about/achievement';
-import PageTitle from '@/components/legacy/page-title';
 
 export default async function Page() {
-  const learnings = await getLearningsQuery();
-  const certifications = await getCertificationsQuery();
+  const histories = await getHistoriesQuery();
 
   return (
-    <section id="history" className="py-20 px-6">
-      <div className="max-w-4xl mx-auto space-y-12">
-        <PageTitle>Learning & Growth</PageTitle>
-        <Achievement contents={learnings} />
-        <Achievement contents={certifications} />
-      </div>
-    </section>
+    <Section id="history" className="py-20 px-6 max-w-4xl mx-auto space-y-12">
+      <SectionTitle className="text-3xl text-center">
+        Learning & Growth
+      </SectionTitle>
+      <Achievement contents={histories} />
+    </Section>
   );
 }
