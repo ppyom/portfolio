5d130b7aec128c58f6bedcfe795da730b159f8dd
ppyom <ouob77@kakao.com>
2026-06-21 20:26:23 +0900

    ♻️ 로그인 페이지 리팩토링

--- app/(application)/login/page.tsx
+++ app/(application)/login/page.tsx
@@ -1,11 +1,12 @@
-import { Suspense } from 'react';
-
 import { LoginForm } from '@/components/login/login-form';
 
 export default function Page() {
   return (
-    <Suspense fallback={null}>
-      <LoginForm />
-    </Suspense>
+    <div className="min-h-150 flex items-center justify-center px-4 pb-24">
+      <div className="w-full max-w-sm space-y-8">
+        <p className="text-2xl font-bold text-center">로그인</p>
+        <LoginForm />
+      </div>
+    </div>
   );
 }
